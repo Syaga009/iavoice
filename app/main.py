dfad30cd97752e28a3b26837d833de82114fa27f
syaga2409 <jyaga24@gmail.com>
2023-09-12 22:53:37 -0500

    c

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-import os
-import numpy as np
-import sounddevice as sd
-import soundfile as sf
-from tensorflow import keras
-import librosa
-import audioread
+#import os
+#import numpy as np
+#import sounddevice as sd
+#import soundfile as sf
+#from tensorflow import keras
+#import librosa
+#import audioread
 from flask import Flask, request, render_template
 app= Flask(__name__)
 
